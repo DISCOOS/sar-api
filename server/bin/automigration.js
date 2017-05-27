1711c2f753f98ae242336fc5b81d4d34810468c3
stianmorsund <stian.rm@gmail.com>
2017-05-27 21:07:34 +0200

    Fixed tracking <-> missionresponse relation

--- server/bin/automigration.js
+++ server/bin/automigration.js
@@ -8,56 +8,68 @@ Automigrate all models
 
 */
 
-dataSource.automigrate('Alarm', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+if (dataSource.connected) {
+	migrate();
+} else {
+	dataSource.once('connected', function () {
+		migrate();
+	});
+}
+
+function migrate() {
+
+	dataSource.automigrate('Alarm', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
 
-dataSource.automigrate('MissionResponse', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('MissionResponse', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('Expence', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('Expence', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('Mission', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('Mission', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('SARUser', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('SARUser', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('Tracking', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('Tracking', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('AlarmUser', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('AlarmUser', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('User', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
-dataSource.automigrate('AccessToken', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('User', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
+	dataSource.automigrate('AccessToken', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
 
-dataSource.automigrate('ACL', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
-dataSource.automigrate('RoleMapping', function (err) {
-	if (err) throw err;
-	dataSource.disconnect();
-});
+	dataSource.automigrate('ACL', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
+	dataSource.automigrate('RoleMapping', function (err) {
+		if (err) throw err;
+		dataSource.disconnect();
+	});
+
+}
