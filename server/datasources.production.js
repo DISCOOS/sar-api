8384ece1a66774fa7047b964505241e8b737f32e
stianmorsund <stian.rm@gmail.com>
2017-05-27 13:19:37 +0200

    Postgresql datasource

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,13 +1,7 @@
 module.exports = {
-  mysql: {
-
-    host: process.env.MYSQL_HOST || 'localhost',
-    database: process.env.MYSQL_DB || 'sar',
-    port: process.env.MYSQL_PORT || 8889,
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PW || 'root',
-    url: "",
-    name: "mysql",
-    connector: "mysql"
+  postgresql: {
+    url: process.env.DATABASE_URL,
+    name: "postgresql",
+    connector: "postgresql"
   }
 };  
