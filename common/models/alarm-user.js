2a42a3dd64c72920d61ba93c6ee3acbf56cf1c42
stianmorsund <stian.rm@gmail.com>
2017-06-02 01:08:55 +0200

    reverted some changes

--- common/models/alarm-user.js
+++ common/models/alarm-user.js
@@ -1,38 +1,6 @@
 'use strict';
 
-var async = require('async');
-var app = require('../../server/server');
 
 module.exports = function (Alarmuser) {
-
-
-    /**
-     * Append saruser to alarmuser response
-     */
-    
-    Alarmuser.afterRemote('find', function (ctx, remoteMethodOutput, next) {  
-        if (!ctx.result) { next(); }
-            if (Array.isArray(ctx.result)) {
-                ctx.result.forEach(function (result) {
-                    if (result.sarUserId) {
-                        
-                        app.models.SARUser.findById(result.sarUserId)
-                            .then(saruser => {
-                                result.sarUser = saruser;
-                                next();
-                            })
-                    }
-
-                });
-            } else {
-                if (ctx.result.sarUserId) {
-                    app.models.SARUser.findById(ctx.esult.sarUserId)
-                        .then(saruser => {
-                            ctx.result.sarUser = saruser;
-                            next();
-                        })
-                }
-            }
-    });
 
 }
