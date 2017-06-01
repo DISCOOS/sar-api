66d4fec84f7e6d217bf416e7a35beeedb7523f0b
stianmorsund <stian.rm@gmail.com>
2017-06-02 00:03:01 +0200

    added afterremote method on alarmuser to expand saruser in response

--- common/models/alarm-user.js
+++ common/models/alarm-user.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Alarmuser) {
+var async = require('async');
 
-};
+module.exports = function (Alarmuser) {
+
+
+    /**
+     * Append saruser to alarmuser response
+     */
+    Alarmuser.afterRemote('find', function (context, remoteMethodOutput, next) {
+        if (ctx.result) {
+            if (Array.isArray(ctx.result)) {
+                ctx.result.forEach(function (result) {
+                    if (result.sarUserId) {
+                        app.models.Saruser.findById(result.sarUserId)
+                            .then(saruser => {
+                                result.sarUser = saruser;
+                                delete result.sarUserId;
+                            })
+                    }
+
+                });
+            } else {
+                if (ctx.result.sarUserId) {
+                    app.models.Saruser.findById(ctx.esult.sarUserId)
+                        .then(saruser => {
+                            ctx.result.sarUser = saruser;
+                            delete ctx.result.sarUserId;
+                        })
+                }
+            }
+        }
+        next();
+
+    });
+
+}
