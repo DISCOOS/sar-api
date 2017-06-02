0c38a2c4efb230294ce164912b4a12eb7159a6f4
stianmorsund <stian.rm@gmail.com>
2017-06-02 21:35:04 +0200

    append saruser to expences

--- common/models/expence.js
+++ common/models/expence.js
@@ -1,5 +1,37 @@
 'use strict';
 
-module.exports = function(Expence) {
+var async = require('async');
+var app = require('../../server/server');
 
-};
+module.exports = function (Expence) {
+
+
+ 
+    
+    Expence.afterRemote('find', function (ctx, remoteMethodOutput, next) {  
+        if (!ctx.result) { next(); }
+            if (Array.isArray(ctx.result)) {
+                ctx.result.forEach(function (result) {
+                    if (result.sarUserId) {
+                        
+                        app.models.SARUser.findById(result.sarUserId)
+                            .then(saruser => {
+                                result.sarUser = saruser;
+                                // her må du ha doneFind og async foreach
+                                next();
+                            })
+                    }
+
+                });
+            } else {
+                if (ctx.result.sarUserId) {
+                    app.models.SARUser.findById(ctx.esult.sarUserId)
+                        .then(saruser => {
+                            ctx.result.sarUser = saruser;
+                            next();
+                        })
+                }
+            }
+    });
+
+}
