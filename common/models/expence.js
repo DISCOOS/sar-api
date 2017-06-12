dbea9df186ed910be7c650d8dbfd9480ccf3c343
stianmorsund <stian.rm@gmail.com>
2017-06-12 12:52:13 +0200

    Saruser added to expense response

--- common/models/expence.js
+++ common/models/expence.js
@@ -8,12 +8,15 @@ module.exports = function (Expence) {
 
     Expence.afterRemote('find', function (ctx, remoteMethodOutput, next) {
         if (remoteMethodOutput.length == 0) { next(); }
+
+        var newResult = [];
         async.forEach(ctx.result, (result, doneFind) => {
             if (result.sARUserId) {
                 app.models.SARUser.findById(result.sARUserId)
                     .then(saruser => {
                         console.log("found saruser")
                         result.sarUser = saruser;
+                        newResult.push(result);
                         doneFind()
                     })
                     .catch((err) => {
@@ -25,6 +28,7 @@ module.exports = function (Expence) {
 
         }, (err) => {
         })
+        ctx.result = newResult;
         next();
     });
 
